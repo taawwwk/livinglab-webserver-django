a36ca7bf0d0f9b85585839b558294dd1d8b24bd2
Kim Taewook <tw.kim.sep@gmail.com>
2026-06-26 16:07:33 +0900

    Fix URL patterns with trailing slashes

--- sensor/urls.py
+++ sensor/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('sensing', views.sensor_sensing, name='sensor_sensing'),
+    path('sensing/', views.sensor_sensing, name='sensor_sensing'),
     path('api/map', views.sensor_map, name='sensor_map'),
     path('new/sensor/', views.latest_sensor_data, name='latest_sensor_data'),
     path('new/ip', views.latest_sensor_ips, name='latest_sensor_ips'),
